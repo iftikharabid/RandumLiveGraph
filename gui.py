d3f9c1a43dd96b0212e6a6ca2455645f978c7254
iftikharabid <mriftikhar@engineer.com>
2017-09-12 17:59:41 -0500

    changes to __name__ by removing QTcore word. working...

--- gui.py
+++ gui.py
@@ -90,8 +90,8 @@ class Ui_MainWindow(object):
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
